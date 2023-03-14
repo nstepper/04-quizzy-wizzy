4b7ed678ce81a55f378f82a2128021f0963aa29e
Nathan Stepper <nstepper@uw.edu>
2023-03-13 18:08:59 -0700

    added some little things and fixed code up

---
+++ assets/js/testQuestions.js
@@ -0,0 +1,28 @@
+// Questions as an array of Objects
+var testQuestions = [
+    {
+      title: 'Commonly used data types DO NOT include:',
+      choices: ['Object', 'Undefined', 'Promises', 'Numbers'],
+      answer: 'Promises'
+    },
+    {
+      title: 'What is not a looping structure in JavaScript?',
+      choices: ['For', 'While', 'Do-While', 'Switch'],
+      answer: 'Switch'
+    },
+    {
+      title: 'What would be the result of 3+2+"7"?',
+      choices: ['12', '57', '14', '32'],
+      answer: '57'
+    },
+    {
+      title: 'Which keyword is used to print the text "Welcome" on the screen?',
+      choices: ['Document.Write(“Welcome);', 'print(“Welcome”);', 'Line.Print(“Welcome”);', 'Console.WriteLine(“Welcome”);'],
+      answer: 'Document.Write(“Welcome);'
+    },
+    {
+      title: 'What is the correct syntax for referring to an external script called “script.js”?',
+      choices: ['<script src=”script.js”>', '<script href=”script.js”>', '<script ref=”script.js”>', '<script name=”script.js”>'],
+      answer: '<script src=”script.js”>'
+    }
+  ];
